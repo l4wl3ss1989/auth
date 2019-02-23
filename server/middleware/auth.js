071236dba907cf1dc56e279c1581b496de5d414a
unknown <l4wl3ss1989@gmail.com>
2019-02-24 00:39:09 +0100

    test

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -6,7 +6,6 @@ let auth = (req,res,next) => {
     User.findByToken(token,(err,user)=> {
         if(err) throw err;
         if(!user) res.status(401).send('no access');
-
         req.token = token;
         next();
     })
